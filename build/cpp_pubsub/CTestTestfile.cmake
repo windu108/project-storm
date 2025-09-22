8d4f82751f5c953679fc2c480887e804a5110b92
Ethan <84253227+windu108@users.noreply.github.com>
2025-09-22 19:08:41 -0400

    Delete build directory

--- build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /project-storm/src/cpp_pubsub
-# Build directory: /project-storm/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
